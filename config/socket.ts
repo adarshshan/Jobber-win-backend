bedef2e87c0f8c157640e6c7b8f779f3cb9e0cc7
Adarsh <adarshshanu3@gmail.com>
2024-05-15 00:50:13 +0530

    socket io implementation completed

--- config/socket.ts
+++ config/socket.ts
@@ -12,14 +12,39 @@ function socketServer(server: any) {
 
     io.on('connection', (socket: Socket) => {
 
-
-        socket.on('sendMessage', (message: any) => {
-            io.emit('message', message);
+        console.log("Connected to socket.io");
+        var u: any
+        socket.on("setup", (userData: any) => {
+            socket.join(userData._id);
+            u = userData._id
+            socket.emit("connected");
         });
 
-        socket.on('disconnect', () => {
+        socket.on('join chat', (room) => {
+            socket.join(room);
+            console.log('user joined Room: ' + room);
+        })
+
+        socket.on("typing", (room) => socket.in(room).emit("typing"));
+        socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+
+        socket.on("new message", (newMessageReceived) => {
+            var chat = newMessageReceived.chat;
+
+            if (!chat.users) return console.log('chat.users not defined');
+
+            chat.users.forEach((user: any) => {
+                if (user._id == newMessageReceived.sender._id) return;
+
+                socket.in(user._id).emit("message recieved", newMessageReceived);
+            })
+        })
+
+        socket.off("setup", () => {
             console.log('User disconnected');
-        });
+            socket.leave(u);
+        })
     });
 
 
