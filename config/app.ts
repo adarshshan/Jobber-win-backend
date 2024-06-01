e9e02f56c9bdf77843c16c9e7aba87395adc3ce3
Adarsh <adarshshanu3@gmail.com>
2024-06-01 11:54:30 +0530

    cors trouble

--- config/app.ts
+++ config/app.ts
@@ -13,7 +13,7 @@ export const createServer = () => {
     try {
         const app: Application | undefined = express()
         const corsOptions = {
-            origin: process.env.CORS_URL,
+            origin: 'https://jobber-win-frontend-w2kd.vercel.app/',
             credentials: true,
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
             allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
