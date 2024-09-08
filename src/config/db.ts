44e4c36bcbe6f84f7a991c5e5ea1664fa60d757c
Adarsh <adarshshanu3@gmail.com>
2024-09-08 20:41:07 +0530

    checking env data

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,11 @@
-import mongoose from 'mongoose'; 
+import mongoose from 'mongoose';
 import { config } from 'dotenv';
 
 config()
 
 const dbUri: string = process.env.DB_URI || '';
+console.log(process.env);
+console.log('this is the env datas...');
 const connectDB = (): void => {
     try {
         mongoose.connect(dbUri).then(() => console.log(`mongodb is connected`));
