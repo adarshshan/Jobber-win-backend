4bc56dbd5e296b8a3226117ac3eae073a5ae8eff
adarsh <adarshshanu3@gmail.com>
2025-12-11 23:40:30 +0530

    opt - parsing using nodemailer is done

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -3,55 +3,51 @@ import { Socket } from "socket.io";
 const { Server } = require("socket.io");
 
 function socketServer(server: any) {
-    const io = new Server(server, {
-        cors: {
-            origin: [process.env.CORS_URL],
-            methods: ['GET', 'POST']
-        }
+  const io = new Server(server, {
+    cors: {
+      origin: [process.env.CORS_URL],
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket: Socket) => {
+    console.log("Connected to socket.io");
+    var u: any;
+    socket.on("setup", (userData: any) => {
+      socket.join(userData?._id);
+      u = userData?._id;
+      socket.emit("connected");
     });
 
-    io.on('connection', (socket: Socket) => {
-
-        console.log("Connected to socket.io");
-        var u: any
-        socket.on("setup", (userData: any) => {
-            socket.join(userData._id);
-            u = userData._id
-            socket.emit("connected");
-        });
-
-        socket.on('join chat', (room) => {
-            socket.join(room);
-            console.log('user joined Room: ' + room);
-        })
-
-        socket.on("typing", (room) => socket.in(room).emit("typing"));
-        socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-
-
-        socket.on("new message", (newMessageReceived) => {
-            var chat = newMessageReceived.chat;
-            if (!chat.users) return console.log('chat.users not defined');
-
-            chat.users.forEach((user: any) => {
-                if (user._id == newMessageReceived.sender._id) return;
-
-                socket.in(user._id).emit("message recieved", newMessageReceived);
-            })
-        })
-
-        socket.on("new notifications", (newNotifications, userId) => {
-            console.log('new notifications are arrived...');
-            socket.in(userId).emit('receivedNotifications', newNotifications);
-        })
-
-        socket.off("setup", () => {
-            console.log('User disconnected');
-            socket.leave(u);
-        })
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("user joined Room: " + room);
     });
 
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
+    socket.on("new message", (newMessageReceived) => {
+      var chat = newMessageReceived.chat;
+      if (!chat.users) return console.log("chat.users not defined");
+
+      chat.users.forEach((user: any) => {
+        if (user?._id == newMessageReceived.sender?._id) return;
+
+        socket.in(user?._id).emit("message recieved", newMessageReceived);
+      });
+    });
+
+    socket.on("new notifications", (newNotifications, userId) => {
+      console.log("new notifications are arrived...");
+      socket.in(userId).emit("receivedNotifications", newNotifications);
+    });
+
+    socket.off("setup", () => {
+      console.log("User disconnected");
+      socket.leave(u);
+    });
+  });
 }
 
 export default socketServer;
--- src/utils/otpGenerator.ts
+++ src/utils/otpGenerator.ts
@@ -1,45 +1,41 @@
-import nodemailer from 'nodemailer'
-import dotenv from 'dotenv';
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.TRANSPORTER_EMAIL,
-        pass: process.env.TRANSPORTER_PASS,
-    }
-})
+  service: "gmail",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.TRANSPORTER_EMAIL,
+    pass: process.env.TRANSPORTER_PASS,
+  },
+});
 
-export const generateAndSendOTP = async (toEmail: string): Promise<string | void> => {
-    console.log('its hitting inside here...');
-    try {
-        const otp: string | null = generateRandomOTP()
-        console.log(`opt is : ${otp}`)
-        const mailOptions = {
-            from: process.env.TRANSPORTER_EMAIL,
-            to: toEmail,
-            subject: 'OTP Verification',
-            text: `Welcome to JobberWin. Your OTP for registration is: ${otp}`
-        }
-        console.log('mailOptions')
-        console.log(mailOptions)
-        const info = await transporter.sendMail(mailOptions);
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        return otp
-    } catch (error) {
-        console.log('hitting here............................')
-        console.log(error as Error)
-    }
-
-}
+export const generateAndSendOTP = async (
+  toEmail: string
+): Promise<string | void> => {
+  try {
+    const otp: string | null = generateRandomOTP();
+    const mailOptions = {
+      from: process.env.TRANSPORTER_EMAIL,
+      to: toEmail,
+      subject: "OTP Verification",
+      text: `Welcome to JobberWin. Your OTP for registration is: ${otp}`,
+    };
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    return otp;
+  } catch (error) {
+    console.log(error as Error);
+  }
+};
 
 function generateRandomOTP(): string {
-    const otpLength = 6
-    const min = Math.pow(10, otpLength - 1)
-    const max = Math.pow(10, otpLength) - 1
-    const randomOTP = Math.floor(min + Math.random() * (max - min + 1))
-    return randomOTP.toString()
+  const otpLength = 6;
+  const min = Math.pow(10, otpLength - 1);
+  const max = Math.pow(10, otpLength) - 1;
+  const randomOTP = Math.floor(min + Math.random() * (max - min + 1));
+  return randomOTP.toString();
 }
